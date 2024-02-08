d04fa507dc037743e899ae23178ef290b2c0df5c
boxfish <boxfish893@gmail.com>
2024-02-08 22:35:34 +0900

    style: eslintとprettierの競合を解決

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -22,9 +23,9 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
     "valid-jsdoc": "off",
     "import/no-named-as-default-member": "off",
     "import/no-named-as-default": "off",
