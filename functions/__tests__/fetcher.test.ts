e815aeb0c30d2541921efd690c3314a02722cdc1
boxfish <boxfish893@gmail.com>
2024-02-15 00:14:07 +0900

    refactor: fetcher内部でdbを問い合わせる方式を回避

---
+++ functions/__tests__/fetcher.test.ts
@@ -0,0 +1,12 @@
+import fetcher from "../src/scraping/fetcher";
+
+describe("fetcher", () => {
+  test("urlからページを取得する", async () => {
+    const url = "https://anime.nicovideo.jp/program";
+    const lastFetch = 0;
+    const date = new Date();
+    const res = await fetcher(url, lastFetch);
+    expect(res.dom).toMatch(/^<!doctype html>/);
+    expect(res.fetchDate).toBeGreaterThan(date.getTime() - 10);
+  });
+});
--- functions/src/scraping/fetcher.ts
+++ functions/src/scraping/fetcher.ts
@@ -1,34 +1,19 @@
-import {getFirestore} from "firebase-admin/firestore";
-
-const fetcher = async (url: string) => {
-  console.log("fetcher:" + url);
-  const db = getFirestore();
-  const dbConfig = db.collection("dbConfig");
-  const getDbDate = (await dbConfig.doc("LastFetch").get()).data();
-  const LastFetch = getDbDate != undefined ? getDbDate.data : undefined;
-
+const fetcher = async (url: string, lastFetch: number) => {
   const getDate = new Date();
   const now = getDate.getTime();
 
-  if (LastFetch != undefined) {
-    while (new Date().getTime() - Number(LastFetch) < 1000 * 5) {
-      await new Promise((resolve) => setTimeout(resolve, 1));
-    }
+  while (new Date().getTime() - lastFetch < 1000 * 5) {
+    await new Promise((resolve) => setTimeout(resolve, 1));
   }
-
-  const document = {
-    data: now,
-  };
-  await dbConfig.doc("LastFetch").set(document);
 
   try {
     const curl = await fetch(url);
     const page = await curl.text();
     console.log("fetch success");
-    return page;
+    return { dom: page, fetchDate: now };
   } catch (error) {
     console.log("error:" + url + " :" + error);
-    return "error";
+    return { dom: "error", fetchDate: now };
   }
 };
 
