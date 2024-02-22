523c9fdc12b20bdda218c0d91c3fe31d57f76685
boxfish <boxfish893@gmail.com>
2024-02-22 22:24:28 +0900

    fix: lintエラーを解消

--- functions/src/db/firebase.ts
+++ functions/src/db/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
-//import { applicationDefault } from "firebase-admin/app";
+// import { applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import * as dotenv from "dotenv";
 dotenv.config();
